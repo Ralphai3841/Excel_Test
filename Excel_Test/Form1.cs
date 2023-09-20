65525af99a6401d13a6a2ea2513e4e2d8e2cd2be
ralpd <ralpd@LAPTOP-FB9086OP>
2023-09-21 01:33:34 +0300

    Add project files.

---
+++ Excel_Test/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Excel_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection baglanti = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ralpd\OneDrive\Masaüstü\TABLO 2.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;'");
+        void listele()
+        {
+            baglanti.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * from [Sayfa1$]", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("insert into [$Sayfa1] (Sütun1,V m/s,ω rad/sn) values (@P1,@P2,@P3)", baglanti);
+            komut.Parameters.AddWithValue("@P1", textBox1.Text);
+            komut.Parameters.AddWithValue("@P2", textBox2.Text);
+            komut.Parameters.AddWithValue("@P3", textBox3.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Yeni Ders Bilgisi Eklendi");
+            listele();
+        }
+
+
+    }
+}
